81f3027814c45ba39803cb0f883b0cd95842e7f9
MariaMaiese <maria.maiese@correoaiep.cl>
2023-11-29 20:35:12 -0300

    avanzando con cotizaciones controller

--- controllers/cotizaciones.controller.ts
+++ controllers/cotizaciones.controller.ts
@@ -1,2 +1,46 @@
 import { Request, Response } from 'express'; import { DataTypes, Model } from "sequelize";
-import { sequelize } from "../database/config";
+import { cotizacion } from '../models/cotizacion.model';
+
+const cotizacionesGet = async (req: Request, res: Response) => {
+    const cotizaciones: cotizacion[] = await cotizacion.findAll()
+
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: cotizaciones
+    })
+}
+
+const cotizacionesGetById = async (req: Request, res: Response) => {
+    const { id } = req.params
+    const resCotizacion = await cotizacion.findByPk(id);
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: resCotizacion
+    })
+}
+
+const cotizacionesPost = async (req: Request, res: Response) => {
+    const { COT_MENSAJE, COT_CANTIDAD_PARTICIPANTES, CUR_ID, USU_ID } = req.body;
+    await cotizacion.create({
+        COT_FECHA: new Date(),
+        COT_RESPONDIDA: false,
+        COT_FECHA_RESPUESTA: new Date(),
+        COT_MENSAJE: COT_MENSAJE,
+        COT_CANTIDAD_PARTICIPANTES: COT_CANTIDAD_PARTICIPANTES,
+        CUR_ID: CUR_ID,
+        USU_ID: USU_ID
+
+    })
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        message: "Cotización creada"
+    })
+}
+
+module.exports = {
+    cotizacionesGet,
+    cotizacionesGetById
+}

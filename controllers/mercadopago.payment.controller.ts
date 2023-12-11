5907a99f1f855318ba655d8067e47fc92da404d4
EduardoSalasG <eduardo.salasgo@correoaiep.cl>
2023-12-10 23:17:45 -0300

    Create order ok, falta la redirección y captura de datos cuando se finaliza la orden

--- controllers/mercadopago.payment.controller.ts
+++ controllers/mercadopago.payment.controller.ts
@@ -3,36 +3,46 @@ import { transaccion } from '../models/transaccion.model';
 import { PaymentCreateRequest } from 'mercadopago/dist/clients/payment/create/types';
 
 // Step 1: Import the parts of the module you want to use
-import { MercadoPagoConfig, Payment } from 'mercadopago';
+import { MercadoPagoConfig, Payment, Preference } from 'mercadopago';
 
 const accessToken: any = process.env.MERCADOPAGOACCESSTOKEN
 
 
 // Step 2: Initialize the client object
-const client = new MercadoPagoConfig({ accessToken: accessToken });
+
+const client = new MercadoPagoConfig({ accessToken: accessToken, });
 
 const payment = new Payment(client)
+
+const preference = new Preference(client)
 
 const createOrder = async (req: Request, res: Response) => {
 
     console.log(client)
 
-    const { transaction_amount, description, payment_method_id, payer, additional_info } = req.body
+    const { transaction_amount, description, payment_method_id, payer, items } = req.body
 
     // Step 4: Create the request object
-    const body: PaymentCreateRequest = {
+    const body = {
         transaction_amount,
         description,
         payment_method_id,
         payer,
-        additional_info
+        items,
+        back_urls: {
+            failure: 'localhost:8081/mercadopago/failure',
+            pending: 'localhost:8081/mercadopago/pending',
+            success: 'localhost:8081/mercadopago/success'
+        },
+        notification_url: "https://acdc-186-107-107-206.ngrok-free.app/mercadopago/webhook",
+
     };
 
     // Step 5: Make the request
-    payment.create({ body, }).then((respuestamp) => {
+    preference.create({ body }).then((respuestamp) => {
         console.log(respuestamp)
-        res.status(200).json({
-            body: respuestamp
+        return res.status(200).json({
+            init_point: respuestamp.init_point
         })
 
     }).catch(console.log);
@@ -43,46 +53,112 @@ const createOrder = async (req: Request, res: Response) => {
 }
 
 const success = async (req: Request, res: Response) => {
-    const { id } = req.params
+    const { status, payment_type, payment_id } = req.params
 
-    const transaccionById: any = await transaccion.findByPk(id);
+
+    // http://localhost:8081/mercadopago/success?collection_id=1320040463
+    // & collection_status=approved
+    //     & payment_id=1320040463
+    //         & status=approved
+    //             & external_reference=null
+    //                 & payment_type=credit_card
+    //                     & merchant_order_id=14005041469
+    //                         & preference_id=496621230 - c35a0443 - 13bd - 4521 - a327 - d2a1a165ab8a
+    //                             & site_id=MLC
+    //                                 & processing_mode=aggregator
+    //                                     & merchant_account_id=null
 
     res.status(200).json({
         ok: true,
         status: 200,
-        body: transaccionById
+        body: req.params
     })
 }
 
-const getPaymentMethods = async (req: Request, res: Response) => {
-    const { id } = req.params
+const failure = async (req: Request, res: Response) => {
+    const { status, payment_type, payment_id } = req.params
 
-    const transaccionById: any = await transaccion.findByPk(id);
+
+    // http://localhost:8081/mercadopago/success?collection_id=1320040463
+    // & collection_status=approved
+    //     & payment_id=1320040463
+    //         & status=approved
+    //             & external_reference=null
+    //                 & payment_type=credit_card
+    //                     & merchant_order_id=14005041469
+    //                         & preference_id=496621230 - c35a0443 - 13bd - 4521 - a327 - d2a1a165ab8a
+    //                             & site_id=MLC
+    //                                 & processing_mode=aggregator
+    //                                     & merchant_account_id=null
 
     res.status(200).json({
         ok: true,
         status: 200,
-        body: transaccionById
+        body: req.params
     })
 }
 
-const webhook = async (req: Request, res: Response) => {
-    const { ETR_ID,
-        MPA_ID,
-        VEN_ID } = req.body;
+const pending = async (req: Request, res: Response) => {
+    const { status, payment_type, payment_id } = req.params
 
-    await transaccion.create({
-        ETR_ID,
-        MPA_ID,
-        VEN_ID,
-        TRA_FECHA: new Date()
-    })
+
+    // http://localhost:8081/mercadopago/success?collection_id=1320040463
+    // & collection_status=approved
+    //     & payment_id=1320040463
+    //         & status=approved
+    //             & external_reference=null
+    //                 & payment_type=credit_card
+    //                     & merchant_order_id=14005041469
+    //                         & preference_id=496621230 - c35a0443 - 13bd - 4521 - a327 - d2a1a165ab8a
+    //                             & site_id=MLC
+    //                                 & processing_mode=aggregator
+    //                                     & merchant_account_id=null
 
     res.status(200).json({
         ok: true,
         status: 200,
-        message: "Transacción creada"
+        body: req.params
     })
+}
+
+const webhook = async (req: any, res: Response) => {
+
+
+    const { type } = req.query
+
+    console.log('query: ', req.query)
+    console.log('params: ', req.params)
+
+    const id = req.query['data.id'] ? req.query['data.id'] : undefined;
+
+    try {
+        if (type === 'payment') {
+            const info = await payment.get({
+                id,
+            }).then((response => {
+                if (response.api_response.status === 200) {
+                    console.log(response)
+                    res.status(204).json({
+                        ok: true,
+                        status: 200,
+                        message: "WEBHOOK, respuesta del payment.get",
+                        body: response
+                    })
+                }
+            }));
+        }
+    } catch (error) {
+        console.log(error)
+
+        res.status(500).json({
+            ok: false,
+            status: 200,
+            message: "WEBHOOK",
+            body: req.query
+        })
+
+    }
+
 }
 
 
@@ -90,5 +166,6 @@ module.exports = {
     createOrder,
     success,
     webhook,
-    getPaymentMethods
+    failure,
+    pending
 } 
--- routes/mercadopago.payment.routes.ts
+++ routes/mercadopago.payment.routes.ts
@@ -1,8 +1,5 @@
 import { Router } from "express";
-import { validarCampos } from "../middlewares/validar-campos";
-import { } from "../helpers/db-validators";
-import { check, param } from "express-validator";
-const { createOrder, success, webhook } = require('../controllers/mercadopago.payment.controller')
+const { createOrder, success, webhook, failure, pending } = require('../controllers/mercadopago.payment.controller')
 
 
 const router = Router();
@@ -10,9 +7,8 @@ const router = Router();
 router.post('/create-order', createOrder);
 
 router.get('/success', success);
-
-router.get('/get-payment-methods', success);
-
+router.get('/failure', failure);
+router.get('/pending', pending);
 router.post('/webhook', webhook);
 
 module.exports = router;
